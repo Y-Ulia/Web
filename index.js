68561ad6b74004dd3e1a3a322df6473bb9f5d179
U.lia <Y-Ulia@users.noreply.github.com>
2019-11-04 03:09:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ export default class Tool {
   constructor (option) {
     this.options = {
       productName: this.getProductName(), // 产品名称
-      init: this.init, // 需要挂载的dom节点 // 或者一个框架的实例上
+      init: this.init,
       ...option
     }
-    this.stack = [] // 插件集合
+    this.menuList = [] //
   }
 
   getProductName() {
@@ -16,12 +16,16 @@ export default class Tool {
 
   init(cb) {
     if (typeof cb === 'function') {
-      return cb()
+      return cb(this.menuList)
+    }
+    if (cb) {
+      throw new Error('菜单栏配置错误')
     }
     // 内置UI 或者 自定义UI
     var dom = document.createElement('div')
     var body = document.body
     // 样式挂载
+    // 插入this.menuList
     body.appendChild(dom)
     return dom
   }
@@ -39,8 +43,23 @@ export default class Tool {
     }
   }
 
-  render(options) { // 需要界面的插件可用
-    // title/event
-    this.stack.push(options)
+  addMenuItem(options) {
+    if (options.title === undefined) {
+      throw new Error('请配置菜单项title')
+    }
+    if (options.event === undefined) {
+      throw new Error('请配置菜单项title')
+    }
+    if (typeof options.event === 'function') {
+      throw new Error('请配置菜单项点击事件')
+    }
+    if (options.index === undefined) {
+      this.menuList.push(options)
+    }
+    if (typeof options.index === 'number') {
+      this.menuList.splice(options.index, 0, options)
+    }
+    // 去掉undefined项
+    this.menuList = this.menuList.filter(m => m)
   }
 }
